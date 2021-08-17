a2ca6caf2846b1931a8c20706a5b14f6f119a2e1
Fyodor Doletov <doletov.fyodor@yandex.ru>
2021-08-17 16:07:42 +0300

    remove killbar.sh script

--- scripts/scripts/killbar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Use this script to kill DWM bar from bar.sh. Useful when using DWM with display manager which does not terminate all user processes at logout
-
-# look for running bar.sh and kill it
-pid=$(pstree -lp | grep -- -bar.sh)
-pid=${pid##*bar.sh\(}
-pid=${pid%%\)*}
-kill $pid
