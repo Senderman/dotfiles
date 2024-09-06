4e13a48b882af6c997c3e9afd038ef41f43520b8
Fyodor Doletov <doletov.fyodor@yandex.ru>
2024-09-07 01:27:30 +0300

    fix(screen): fix screen directory

--- scripts/scripts/screen-menu.sh
+++ scripts/scripts/screen-menu.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-if [ ! -d "/run/screens/S-${USER}" ]; then
+
+SCREEN_DIR="${HOME}/.screen"
+
+if [ ! -d "${SCREEN_DIR}" ]; then
   notify-send "No screen session directory found!"
   exit 0
 fi
 
-selection="$(ls "/run/screens/S-${USER}" | rofi -dmenu -p screens)"
+selection="$(ls "${SCREEN_DIR}" | rofi -dmenu -p screens)"
 [ -n "$selection" ] && alacritty -e screen -r "$selection"
