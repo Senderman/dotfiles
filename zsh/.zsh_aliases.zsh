2c542c584bab0047a72cfbd61bb2c7d9ec2642ac
Fyodor Doletov <doletov.fyodor@yandex.ru>
2025-03-14 20:43:11 +0300

    increase s6-rc verbosity

--- zsh/.zsh_aliases.zsh
+++ zsh/.zsh_aliases.zsh
@@ -17,7 +17,7 @@ alias git-find-deleted-paths='git log --diff-filter=D --summary | grep delete | 
 alias git-find-deleted-log='git log --oneline --follow --'
 alias f='ranger'
 alias trr='transmission-remote'
-alias s6-rc-user="s6-rc -l $XDG_RUNTIME_DIR/s6/s6-rc"
+alias s6-rc-user="s6-rc -l $XDG_RUNTIME_DIR/s6/s6-rc -v 2"
 alias gen_psw="od -An -N16 -tx /dev/urandom | tr -d ' '"
 
 which doas 1>/dev/null 2>/dev/null && alias sudo='doas'
