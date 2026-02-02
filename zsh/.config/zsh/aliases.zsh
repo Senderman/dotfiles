897133e8d589e623ab22058b05115541cfc15eb1
Fyodor Doletov <doletov.fyodor@yandex.ru>
2026-02-02 18:36:24 +0300

    update zsh aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,40 +1,47 @@
-alias pacstore='pacman -Slq | fzf --multi --preview "pacman -Si {1}" | xargs -ro doas pacman -S'
-alias pacin='doas pacman -S'
-alias pacrem='doas pacman -Rns'
-alias pacrmdeps='doas pacman -Rns $(pacman -Qdtq)'
-alias pacupg='doas pacman -Syu'
-
-alias aurstore='yay -Salq | fzf --multi --preview "yay -Sai {1}" | xargs -ro yay -S'
-alias yain='yay -S'
-alias yaupg='yay -Sau'
-
+# Command behaviour overrides
 alias df='df -h -x tmpfs -x devtmpfs -x efivarfs'
 alias free='free -h'
 alias wget="wget --hsts-file="${XDG_CACHE_HOME}/wget-hsts""
 alias s3cmd="s3cmd -c "${XDG_CONFIG_HOME}/s3/s3cfg""
 alias fuseiso='fuseiso -n'
+alias trr='transmission-remote'
+which kubecolor 1>/dev/null 2>/dev/null && alias kubectl='kubecolor' && compdef kubecolor=kubectl
 
+# Use modern utils
 alias ls='lsd'
 alias l='lsd -l'
 alias la='lsd -a'
 alias lla='lsd -la'
 alias lg='lsd -lg'
 alias lt='lsd --tree'
+alias cat='bat --theme=base16'
+alias diff='delta'
+alias cd='z'
 
-alias e=$EDITOR
+# Git aliases
 alias g='git'
 alias git-find-deleted-paths='git log --diff-filter=D --summary | grep delete | grep'
 alias git-find-deleted-log='git log --oneline --follow --'
 alias git-copybranch='git branch --show-current | wl-copy'
-alias trr='transmission-remote'
-alias bat='bat --theme=base16'
 
-which kubecolor 1>/dev/null 2>/dev/null && alias kubectl='kubecolor' && compdef kubecolor=kubectl
+# Pacman aliases
+alias pacstore='pacman -Slq | fzf --multi --preview "pacman -Si {1}" | xargs -ro doas pacman -S'
+alias pacin='doas pacman -S'
+alias pacrem='doas pacman -Rns'
+alias pacrmdeps='doas pacman -Rns $(pacman -Qdtq)'
+alias pacupg='doas pacman -Syu'
 
+# Yay aliases
+alias aurstore='yay -Salq | fzf --multi --preview "yay -Sai {1}" | xargs -ro yay -S'
+alias yain='yay -S'
+alias yaupg='yay -Sau'
+
+# Screen recording
 alias wfr_silent='wf-recorder -c libx264rgb -F "scale=-2:720" -f'
 alias wfr_mic='wf-recorder -c libx264rgb -a -F "scale=-2:720" -f'
 alias wfr_audio='usertree u pipewire-loopback && wf-recorder -c libx264rgb -aoutput.wfr -F "scale=-2:720" -f'
 
+# Yazi with cwd update
 function f() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
 	yazi "$@" --cwd-file="$tmp"
@@ -44,6 +51,7 @@ function f() {
 	rm -f -- "$tmp"
 }
 
+# Show all terminal colors
 function termcolors() {
     fg='\e[38;5;';bg='\e[48;5;'
     for ix in {1..256}; do
@@ -51,3 +59,4 @@ function termcolors() {
     done
     printf "\e[0m"
 }
+
