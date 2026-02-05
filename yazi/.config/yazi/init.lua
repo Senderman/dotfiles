3c63ebb2bc09ae43647622e8d274af8bc35a96aa
Fyodor Doletov <doletov.fyodor@yandex.ru>
2026-02-05 03:14:51 +0300

    remove yazi flavor, remove bookmarks plugin

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,23 +1,5 @@
 require("git"):setup()
 
-require("bookmarks"):setup({
-	last_directory = { enable = false, persist = false, mode="dir" },
-	persist = "all",
-	desc_format = "parent",
-	file_pick_mode = "parent",
-	custom_desc_input = false,
-	show_keys = true,
-	notify = {
-		enable = true,
-		timeout = 1,
-		message = {
-			new = "New bookmark '<key>' -> '<folder>'",
-			delete = "Deleted bookmark in '<key>'",
-			delete_all = "Deleted all bookmarks",
-		},
-	},
-})
-
 Status:children_add(function()
 	local h = cx.active.current.hovered
 	if not h or ya.target_family() ~= "unix" then
