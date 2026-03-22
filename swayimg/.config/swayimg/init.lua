bb992858ffb5abcfca8d18e7fcf2a7ded52bde4b
Fyodor Doletov <doletov.fyodor@yandex.ru>
2026-03-22 06:25:39 +0300

    update swayimg config

---
+++ swayimg/.config/swayimg/init.lua
@@ -0,0 +1,146 @@
+swayimg.set_mode('viewer')
+swayimg.enable_antialiasing(true)
+swayimg.set_dnd_button('MouseRight')
+
+swayimg.text.set_font('JetBrainsMono Nerd Font')
+swayimg.text.set_size(20)
+
+-- Viewer mode
+swayimg.viewer.set_default_scale('optimal')
+swayimg.viewer.set_default_position('center')
+swayimg.viewer.set_window_background(0xff000000)
+swayimg.viewer.set_image_background(0xffffffff)
+swayimg.viewer.limit_preload(1)
+
+swayimg.viewer.on_key('q', function()
+    swayimg.exit(0)
+end)
+
+swayimg.viewer.on_key('Return', function()
+    swayimg.set_mode('gallery')
+end)
+
+swayimg.viewer.on_key('n', function()
+    swayimg.viewer.switch_image('next')
+end)
+
+swayimg.viewer.on_key('p', function()
+    swayimg.viewer.switch_image('prev')
+end)
+
+swayimg.viewer.on_key('f', function()
+    swayimg.toggle_fullscreen()
+end)
+
+swayimg.viewer.on_key('z', function()
+    swayimg.viewer.reset()
+end)
+
+-- set current image as wallapper
+swayimg.viewer.on_key('w', function()
+    local img = swayimg.viewer.get_image()
+    os.execute('swww img "'..img.path..'"')
+end)
+
+-- delete current image
+swayimg.viewer.on_key('Shift-d', function()
+    local img = swayimg.viewer.get_image()
+    os.execute('rm -- "'..img.path..'"')
+end)
+
+-- zoom image
+swayimg.viewer.on_key('KP_Add', function()
+    local scale = swayimg.viewer.get_scale()
+    swayimg.viewer.set_abs_scale(scale + 0.1)
+end)
+
+swayimg.viewer.on_key('KP_Subtract', function()
+    local scale = swayimg.viewer.get_scale()
+    swayimg.viewer.set_abs_scale(scale - 0.1)
+end)
+
+swayimg.viewer.on_key('KP_Add', function()
+    local scale = swayimg.viewer.get_scale()
+    swayimg.viewer.set_abs_scale(scale + 0.1)
+end)
+
+swayimg.viewer.on_key('KP_Subtract', function()
+    local scale = swayimg.viewer.get_scale()
+    swayimg.viewer.set_abs_scale(scale - 0.1)
+end)
+
+-- h/j/k/l to move image
+swayimg.viewer.on_key('h', function()
+    local wnd = swayimg.get_window_size()
+    local pos = swayimg.viewer.get_position()
+    swayimg.viewer.set_abs_position(math.floor(pos.x + wnd.width / 10), pos.y);
+end)
+
+swayimg.viewer.on_key('l', function()
+    local wnd = swayimg.get_window_size()
+    local pos = swayimg.viewer.get_position()
+    swayimg.viewer.set_abs_position(math.floor(pos.x - wnd.width / 10), pos.y);
+end)
+
+swayimg.viewer.on_key('j', function()
+    local wnd = swayimg.get_window_size()
+    local pos = swayimg.viewer.get_position()
+    swayimg.viewer.set_abs_position(pos.x, math.floor(pos.y - wnd.width / 10));
+end)
+
+swayimg.viewer.on_key('k', function()
+    local wnd = swayimg.get_window_size()
+    local pos = swayimg.viewer.get_position()
+    swayimg.viewer.set_abs_position(pos.x, math.floor(pos.y + wnd.width / 10));
+end)
+
+-- rotate image
+swayimg.viewer.on_key('bracketleft', function()
+    swayimg.viewer.rotate(270)
+end)
+
+swayimg.viewer.on_key('bracketright', function()
+    swayimg.viewer.rotate(90)
+end)
+
+-- flip image
+swayimg.viewer.on_key('m', function()
+    swayimg.viewer.flip_vertical()
+end)
+
+swayimg.viewer.on_key('Shift-m', function()
+    swayimg.viewer.flip_horizontal()
+end)
+
+
+-- Gallery mode
+swayimg.gallery.on_key('q', function()
+    swayimg.exit(0)
+end)
+
+swayimg.gallery.on_key('Return', function()
+    swayimg.set_mode('viewer')
+end)
+
+swayimg.gallery.on_key('h', function()
+    swayimg.gallery.switch_image('left')
+end)
+
+swayimg.gallery.on_key('l', function()
+    swayimg.gallery.switch_image('right')
+end)
+
+swayimg.gallery.on_key('j', function()
+    swayimg.gallery.switch_image('down')
+end)
+
+swayimg.gallery.on_key('k', function()
+    swayimg.gallery.switch_image('up')
+end)
+
+-- set current image as wallapper
+swayimg.gallery.on_key('w', function()
+    local img = swayimg.gallery.get_image()
+    os.execute('swww img "'..img.path..'"')
+end)
+
