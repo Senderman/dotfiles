7b8168193491bb1fe8cfd6f74ec7b9eeda037b79
Fyodor Doletov <doletov.fyodor@yandex.ru>
2026-05-09 02:49:35 +0300

    yazi - update ratio, and mtime to status

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,15 +1,26 @@
+-- Init for yazi-git plugin
 require("git"):setup()
 
+-- Add uid:gid and mtime to status line
 Status:children_add(function()
     local h = cx.active.current.hovered
     if not h or ya.target_family() ~= "unix" then
         return ""
     end
 
+    local time = math.floor(h.cha.mtime or 0)
+    if time == 0 then
+        time = ""
+    else
+        time = os.date("%d.%m.%Y", time)
+	end
+
     return ui.Line {
         ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
         ":",
         ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+        " ",
+        ui.Span(time):fg("yellow"),
         " ",
     }
 end, 500, Status.RIGHT)
